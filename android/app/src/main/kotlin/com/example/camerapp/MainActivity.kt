407e816b98693e7ec1ff66467ffdb12bf26d085a
Cooldude4973 <cooldude4973@gmail.com>
2023-09-14 12:49:39 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/camerapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.camerapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:camerapp/MyApp.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: const MyApp()));
+}
